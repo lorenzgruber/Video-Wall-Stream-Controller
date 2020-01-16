52d524f4959387442303735183e643a2f7af0ba0
gruberlorenz <regru.lg@gmail.com>
2020-01-16 10:53:10 +0100

    styled buttons

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 let isStreamChangeable = true;
+let streamIndex = 0;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/controller.html');
@@ -10,12 +11,13 @@ app.get('/', (req, res) => {
 http.listen(3000, () => console.log('listening on *:3000'));
 
 io.on('connection', (socket) => {
-    socket.emit('join', isStreamChangeable);
+    socket.emit('join', {isStreamChangeable, streamIndex});
     socket.on('change-stream-buttons', data => {
-        if(isStreamChangeable) {
+        if(isStreamChangeable && streamIndex != data) {
+            streamIndex = data;
             isStreamChangeable = false;
-            io.emit('stream-closed');
-            io.emit('change-stream', data);
+            io.emit('stream-closed', streamIndex);
+            io.emit('change-stream', streamIndex);
         }
     });
     
